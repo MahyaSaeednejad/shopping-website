aa874d6cba12c0fecfc78df46850e74dda7a910f
MahyaSaeednejad <msaeednejad94@gmail.com>
2021-01-11 08:08:38 -0600

    created shopping website and the model for products

---
+++ Shopping.Website/Shopping.Website/Model/product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Shopping.Website.Model
+{
+    public class product
+    {
+        [JsonPropertyName("Id")]
+        public String Id { get; set; }
+        
+        [JsonPropertyName("Maker")]
+        public String Maker { get; set; }
+        
+        [JsonPropertyName("img")]
+        public String Image { get; set; }
+        
+        [JsonPropertyName("Url")]
+        public  String URL { get; set; }
+        
+        [JsonPropertyName("Title")]
+        public String Title { get; set; }
+        
+        [JsonPropertyName("Description")]
+        public String Description { get; set; }
+        
+        [JsonPropertyName("Ratings")]
+        public int[] Rating { get; set; }
+    }
+}
